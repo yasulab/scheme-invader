d5bc4b7448943511dba886aec7d16d34d42e671f
Yohei Yasukawa <yasu1000@gmail.com>
2010-11-30 10:58:44 -0600

    First commit

---
+++ test.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname test) (read-case-sensitive #t) (teachpacks ((lib "image.ss" "teachpack" "htdp") (lib "universe.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.ss" "teachpack" "htdp") (lib "universe.ss" "teachpack" "2htdp")))))
+(define hoge
+  (make-posn 3 2))
+(posn-x hoge)
+(posn-y hoge)
+
